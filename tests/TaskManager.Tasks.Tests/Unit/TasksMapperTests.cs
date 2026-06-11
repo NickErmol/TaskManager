b077126f8cfcfa4ba3f7ac5216ecbde76c0b7a06
Nickerm <70840188+NickErmol@users.noreply.github.com>
2026-06-11 11:21:29 +0200

    test(tasks): assert first checklist item position in mapper test

--- tests/TaskManager.Tasks.Tests/Unit/TasksMapperTests.cs
+++ tests/TaskManager.Tasks.Tests/Unit/TasksMapperTests.cs
@@ -20,6 +20,7 @@ public class TasksMapperTests
         dto.Checklist[0].Id.Should().Be(a.Id);
         dto.Checklist[0].Title.Should().Be("first");
         dto.Checklist[0].IsDone.Should().BeFalse();
+        dto.Checklist[0].Position.Should().Be(0);
         dto.Checklist[1].Id.Should().Be(b.Id);
         dto.Checklist[1].IsDone.Should().BeTrue();
         dto.Checklist[1].Position.Should().Be(1);
